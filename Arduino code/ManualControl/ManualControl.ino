7ff0a1216bb9c600fb2a79b5a67f505ad15f219c
Frank-JJ <frankjuuljensen1@gmail.com>
2024-04-10 00:21:27 +0200

    Updated arduino code to handle arbitrary amount of servos at the same time, currently set to 3. Updated keyboardInput.py with example code.

--- Arduino code/ManualControl/ManualControl.ino
+++ Arduino code/ManualControl/ManualControl.ino
@@ -14,35 +14,43 @@ Servo can have pins >= 2, since 0 and 1 are interfered with by Serial connection
 */
 
 #include <Servo.h>
-Servo myservo;  // create servo object to control a servo 
+#define NUM_SERVOS 3
+Servo servo[NUM_SERVOS]; // create servo object to control servos
+uint8_t servoPins[NUM_SERVOS] = {2,3,4};
+uint8_t servoVals[NUM_SERVOS] = { }; // Initializes to zero
 
 void setup() {
-    Serial.begin(115200);
-    myservo.write(0); // Initial servo angle
-    myservo.attach(2);  // Servo pin
+    Serial.begin(115200, SERIAL_8N1); // Begin serial connection with 115200 Baud Rate, 8 data bits, no parity, one stop bit
+    for (size_t i = 0; i < NUM_SERVOS; i++)
+    {
+        servo[i].write(0); // Initial servo angle
+        servo[i].attach(servoPins[i]);  // Servo pin
+    }
+    
     Serial.println("ManualControl started!");
 }
 
 void loop() {
-    String readString;
     
-    // Get data from serial port
-    while (Serial.available()) {
-        char c = Serial.read();  // Get one byte from serial buffer
-        readString += c; // Make the string readString
-        delay(2);  // Slow dowm looping to allow buffer to fill with next character
-    }
-
-    // Handle data
-    if (readString.length() > 0) {
-        // Write back a copy of what was recieved
+    // Check for data in serial connection buffer
+    if (Serial.available() >= NUM_SERVOS) {
+        uint32_t c = Serial.readBytes(servoVals, NUM_SERVOS);  // Get NUM_SERVOS bytes from serial buffer
+        
+        // Handle data and send response back through serial connection
         Serial.print("Recieved: ");
-        Serial.println(readString);
-        int n = readString.toInt();
+        for (size_t i = 0; i < NUM_SERVOS; i++)
+        {
+            Serial.print(servoVals[i]);
+            Serial.print(" | ");
+        }
 
-        // Write position to servo
         Serial.print("Setting angle: ");
-        Serial.println(n);
-        myservo.write(n);
-    } 
+        for (size_t i = 0; i < NUM_SERVOS; i++)
+        {
+            Serial.print(servoVals[i]);
+            Serial.print(" | ");
+            servo[i].write(servoVals[i]); // Set servo angles
+        }
+        Serial.println("");
+    }
 }
--- Arduino code/ManualControl/keyboardInput.py
+++ Arduino code/ManualControl/keyboardInput.py
@@ -3,30 +3,51 @@ from pynput import keyboard
 from timeit import default_timer as timer
 
 # Configure your COM port and baud rate
-COM_PORT = '/dev/ttyACM0'  # Change this to your COM port
+COM_PORT = 'COM3'  # Change this to your COM port
 BAUD_RATE = 115200   # Change this to match your UART device configuration
 
-# Open the serial connection
-ser = serial.Serial(COM_PORT, BAUD_RATE)
+# Open the serial connection wtih com pot and baud rate, as well as 8 data bits, no parity, one stop bit
+ser = serial.Serial(COM_PORT, BAUD_RATE, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
 
-pos = 0
-ser.write(str(int(pos)).encode('utf-8'))
+# Control keys (up, down, up, down, up, down)
+# These are used for moving servo "up" or "down"
+# 'q' and 'a' are for moving servo 1 "up" and "down", and so on...
+control_keys = [keyboard.KeyCode.from_char(c) for c in 'qawsed']
+
+# Sending an array as a byte array makes it easy to use the data directly as a simple array on the arduino
+def send_as_bytearray(arr):
+    ser.write(bytearray(arr))
+
+# Initialize position array to zero
+pos = [0, 0, 0]
+send_as_bytearray(pos)
 
 # Multiplier to comand constant rotation speed of servo
 # 1 is slow
 vel_mult = 2
 
 def on_press(key):
-    # print("press", key)
-    # if ser.in_waiting == 0:
-    change = 0
-    if key == keyboard.Key.up:
-        change += 1
-    if key == keyboard.Key.down:
-        change -= 1
+    # Initialize change to zero
+    change = [0, 0, 0]
+    
+    # Check key type
+    try:
+        key_pressed = control_keys.index(key)
+    except:
+        # Key not in control_keys
+        return
+    
+    # Set change in position array
+    change[int(key_pressed / 2)] = 1
+    if key_pressed % 2:
+        change[int(key_pressed / 2)] *= -1
+        
     global pos
-    pos += change*vel_mult
-    ser.write(str(int(pos)).encode('utf-8'))
+    # Update position array and send it
+    for index in range(len(change)):
+        if pos[index] + change[index]*vel_mult <= 180 and pos[index] + change[index]*vel_mult >= 0:
+            pos[index] += change[index]*vel_mult
+    send_as_bytearray(pos)
     
 
 def on_release(key):
@@ -37,11 +58,14 @@ listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 
 
 try:
+    # Start listener that allows getting keyboard input
     listener.start()
+    
     while True:
+        # Chech and read messages from serial connection
         if ser.in_waiting > 0:
-            c = ser.readline()
-            print(c.decode('utf-8').replace('\n',''))
+            c = ser.readline().decode()
+            print(c)
 
 except KeyboardInterrupt:
     print("Exiting...")
